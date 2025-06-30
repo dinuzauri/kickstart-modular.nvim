8175f63c43a182a08a93d020e4c45d0f4e349a8c
Dinu Bălan <dinu@ik.me>
2025-06-30 11:12:29 +0300

    Update codestats.lua

--- lua/custom/plugins/codestats.lua
+++ lua/custom/plugins/codestats.lua
@@ -7,7 +7,7 @@ return {
     require('codestats').setup {
       username = 'aMorph', -- needed to fetch profile data
       base_url = 'https://codestats.net', -- codestats.net base url
-      api_key = 'SFMyNTY.WVUxdmNuQm8jI01UUTNPRE09.Wg_BLUmav_31me80FV4YFHI5HfMdmMdyi1T4Og_JTkQ',
+      api_key = '<<CodestatsAPI>>',
       send_on_exit = true, -- send xp on nvim exit
       send_on_timer = true, -- send xp on timer
       timer_interval = 60000, -- timer interval in milliseconds (minimum 1000ms to prevent DDoSing codestat.net servers)
